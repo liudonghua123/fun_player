f816f457280491f586ae15bad3bc375a04ffc991
liudonghua123 <liudonghua123@gmail.com>
2022-11-26 15:18:12 +0800

    update code for java ffi winmm.mciSendStringA

--- src/main/java/com/liudonghua/examples/App.java
+++ src/main/java/com/liudonghua/examples/App.java
@@ -1,13 +1,16 @@
 package com.liudonghua.examples;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+public class App {
+
+    public interface Winmm extends Library {
+        public int mciSendStringA(String lpszCommand, String lpszReturnString, int cchReturn, int hwndCallback);
+    }
+
+    public static void main(String[] args) {
+        Winmm winmm = (Winmm) Native.load("winmm", Winmm.class);
+        System.out.println(winmm.mciSendStringA("play mario.mid wait", null, 0, 0));
     }
 }
